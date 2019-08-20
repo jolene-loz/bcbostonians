2412e0f3b0c8dc861d8fe504f2222469a23e34ec
Jolene Lozano <lozanoja@bc.edu>
2019-08-20 17:43:41 -0400

    redid audio url

--- js/circleaudioplayer.js
+++ js/circleaudioplayer.js
@@ -298,7 +298,7 @@ CircleAudioPlayer.prototype = {
 
 // now init one as an example
 var cap = new CircleAudioPlayer({
-	audio: 'file:///Users/jlozano/Downloads/radios/js/testOdesza.m4a',
+	audio: url('js/websiteSong.m4a'),
   size: 120,
   borderWidth: 4
 });
